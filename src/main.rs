07f97274ba49591fbf6e4f691a3f4fbaaa1656db
h-wal <harsh2102agarwal@gmail.com>
2025-11-09 23:31:33 +0530

    added orderbook structs:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap; //used to store the users array in memory
+use std::{collections::{BTreeMap, HashMap, VecDeque}, hash::Hash, vec}; //used to store the users array in memory
 use axum::{
     Json, 
     Router, 
@@ -18,8 +18,6 @@ struct AuthRequest{     //namefeild struct  -   "semantic name - this means ever
     email: String,
     password: String
 }
-
-struct Wrapper<T>(T); // This is called a tuple struct - 
 
 #[derive(Serialize)]
 pub struct AuthResponse {
@@ -69,6 +67,7 @@ impl AuthResponse{                          //we are implementing factory method
 pub struct OnRampResponse {
     pub message: String,
     pub new_balance: u64,
+    pub new_holdings: u64,
     #[serde(skip_serializing)] //we are skipping serializing this status as it can be directly infered by the tcp 
     pub status: StatusCode
 }
@@ -82,12 +81,12 @@ impl IntoResponse for OnRampResponse {        //This is to implement IntoRespons
 
 impl OnRampResponse{
      
-     pub fn ok(msg: impl Into<String>, new_balance: u64) -> Self{
-        Self { message: msg.into() , new_balance: new_balance, status:StatusCode::ACCEPTED }
+     pub fn ok(msg: impl Into<String>, new_balance: u64, new_holdings: u64) -> Self{
+        Self { message: msg.into() , new_balance: new_balance, new_holdings: new_holdings, status: StatusCode::ACCEPTED }
      }
 
-     pub fn err(msg: impl Into<String>, new_balance: u64) -> Self{
-        Self { message: msg.into(), new_balance, status: status::StatusCode::INTERNAL_SERVER_ERROR }
+     pub fn err(msg: impl Into<String>, new_balance: u64, new_holdings: u64) -> Self{
+        Self { message: msg.into(), new_balance, new_holdings: new_holdings ,status: status::StatusCode::INTERNAL_SERVER_ERROR }
      }
 }
 struct SignupResponseType {
@@ -112,33 +111,123 @@ enum DbCommand {
         response_status: oneshot::Sender<SigninResponseType>
     },
     OnRamp {
-        delta_balance: u64,
         user_email: String,
+        delta_balance: u64,
+        delta_holdings: u64,
         response_status: oneshot::Sender<OnRampDbResponseType>
     }
     // UpdateBalance { email: String, amount: u32 },
 }
 
+#[derive(Deserialize)]
+enum Side {
+    Bid,
+    Ask,
+}
+
+struct Trade {
+    buyer: String,
+    seller: String,
+    qty: u64,
+    price : u64
+}
+struct OrderSummary {
+    owner: String,
+    qty: u64,
+    price: u64,
+    side: Side,      // Reuse Side::Bid / Side::Ask
+}
+struct OrderbookResponse {
+    status: String,
+    fills: Vec<Trade> , //change to trade later,
+    remaining_qty: u64,
+    bids: Option<Vec<OrderSummary>>,
+    asks: Option<Vec<OrderSummary>>
+}
+enum OrderbookCommand {
+    NewLimitOrder{
+        market_id: String,
+        user_id: String,
+        side: Side,
+        qty: u64,
+        price: u64,
+        resp: oneshot::Sender<OrderbookResponse> 
+    },
+    NewMarketOrder{
+        market_id: String,
+        user_id: String,
+        side: Side,
+        qty: u64,
+        resp: oneshot::Sender<OrderbookResponse>
+    },
+    GetBook{
+        market_id: String,
+        resp: oneshot::Sender<OrderbookResponse>
+    }
+}
+#[derive(Deserialize)]
 struct Order{
     qty: u32,
-    price: u32
+    price: u32,
+    side: Side
 }
 struct User{
     email: String,
     password: String,
     balance: u64,
-    holding: u32
+    holdings: u64
 }
+
+struct MarketBook {
+    bids: BTreeMap<u64, VecDeque<Order>>,
+    asks: BTreeMap<u64, VecDeque<Order>>,
+}
+
+// impl MarketBook {
+
+//     fn new() -> Self{
+
+//     }
+
+//     fn inser_order(&mut self, order: Order) -> {
+
+//     }
+
+//     async fn match_order(&mut self, order: Order, db_tx: &DbSender) -> OrderbookResponse{
+//         trades = [];
+//         if order.side == "Bid" {
+
+//         }
+
+//     }
+
+
+//     fn snapshot(&self) -> (Vec<OrderSummary>, Vec<OrderSummary>);
+// }
 
 struct OnRampDbResponseType{
     status: String,
-    balance: u64
+    balance: u64,
+    holdings: u64
 }
 
 #[derive(Deserialize)]
 pub struct OnRampHttpRequest{
     pub user_email: String,
-    pub balance: u64
+    pub balance: u64,
+    pub holding: u64
+}
+
+#[derive(Deserialize)]
+pub struct CreateMarketOrderRequest{
+    pub user_email: String,
+    pub order: Order,
+}
+
+pub struct CreateMarketOrderResponse{
+    message: String,
+    trades: Vec<Trade>,
+    pub status: StatusCode
 }
 
 // impl IntoResponse for OnRampHttpRequest {        //This is to implement IntoResponse functionality so that axum can use it in http body
@@ -152,7 +241,7 @@ pub struct OnRampHttpRequest{
 async fn user_db_actor(mut rx: mpsc::Receiver<DbCommand>){
     let mut users: HashMap<String, User> = HashMap::new();
 
-    println!("UserDBActor started...");
+    println!("UserDBActor started");
 
     // Infinite loop — actor waits for incoming messages
     while let Some(cmd) = rx.recv().await {
@@ -169,7 +258,7 @@ async fn user_db_actor(mut rx: mpsc::Receiver<DbCommand>){
                         email: email.clone(),
                         password,
                         balance: 0,
-                        holding: 0
+                        holdings: 0
                     };
                     users.insert(email.clone(), user);
                     let _ = response_status.send(SignupResponseType {
@@ -200,15 +289,28 @@ async fn user_db_actor(mut rx: mpsc::Receiver<DbCommand>){
 
                 let _ = response_status.send(response);
             },
-            DbCommand::OnRamp { delta_balance, user_email , response_status} => {
+            DbCommand::OnRamp { user_email , delta_balance, delta_holdings,response_status} => {
                 let status: OnRampDbResponseType = if let Some(user) = users.get_mut(&user_email){ //users.get_mut --> returns a mutable reference to the users hashmap
                     user.balance += delta_balance;
-                    OnRampDbResponseType { status: format!("Successfull! User {} now has {} ", user.email, user.balance), balance: user.balance }
+                    user.holdings += delta_holdings;
+                    OnRampDbResponseType { status: format!("Successfull! User {} now has balance : {} , holding: {} ", user.email, user.balance, user.holdings), balance: user.balance, holdings: user.holdings }
                 } else {
-                    OnRampDbResponseType { status: format!("User not found! User: {} found", user_email).to_string(), balance: 0}
+                    OnRampDbResponseType { status: format!("User not found! User: {} found", user_email).to_string(), balance: 0, holdings: 0}
                 };
                 let _ = response_status.send(status);
             }
+        }
+    }
+}
+
+async fn orderbook_actor(mut rx: mpsc::Receiver<OrderbookCommand>){
+    let mut Orderbook: HashMap<String, MarketBook>= HashMap::new();
+
+    println!("MarketBookDbActor Started");
+
+    while let Some(cmd) = rx.recv().await{
+        match cmd {
+            
         }
     }
 }
@@ -267,34 +369,47 @@ async fn onramp_function(
     let update_userbalance = db_tx.send(DbCommand::OnRamp{
         user_email: payload.user_email.clone(),
         delta_balance: payload.balance,
+        delta_holdings: payload.holding,
         response_status: oneshot_tx
     }).await;
     
     match oneshot_rx.await {
         Ok(response) => {
             if response.status.contains("Successfull"){
-                OnRampResponse::ok(response.status, response.balance)
+                OnRampResponse::ok(response.status, response.balance, response.holdings)
             } else {
-                OnRampResponse::err(response.status, response.balance)
+                OnRampResponse::err(response.status, response.balance, response.holdings)
             }
         },
         Err(response) => {
-            OnRampResponse::err("Internal server Error", 0)
+            OnRampResponse::err("Internal server Error", 0, 0)
         } 
     }
 }
 
+async fn create_limit_order_function(
+    State(db_tx): State<DbSender>,
+    Json(payload): Json<CreateMarketOrderRequest>
+) -> CreateMarketOrderResponse{
+    let (oneshot_tx, oneshot_rx) = oneshot::channel();
+    let order_created = 
+
+}
 #[tokio::main]
 async fn main() {
 
     let (tx, rx) = mpsc::channel::<DbCommand>(32); // defining an mpsc channel
     tokio::spawn(user_db_actor(rx));
+    let (tx2, rx2 ) = mpsc::channel::<OrderbookCommand>(32);
+    tokio::spawn(orderbook_actor(rx2));
+    let ob_tx = tx2.clone();
     let db_tx = tx.clone();
     // build our application with a single route
     let app = Router::new().route("/", post(|| async{"Hello World!"}))
     .route("/signup", post(signup_function))
     .route("/signin", post(signin_function))
     .route("/onramp", post(onramp_function)) //this route will return OnRampResponse type of its own which tells back the request handler the updated balance
+    .route("/createLimitOrder", post(create_limit_order_function))
     .with_state(db_tx);
 
     // .route("/create_limit_order", post(create_limit_order_function))
